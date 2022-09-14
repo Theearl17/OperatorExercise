f56004db14da8e3958d234aff29f1dad80ead4ab
earl starks <earlstarks@rocketmail.com>
2022-09-14 08:58:46 -0500

    Operator Exercise

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 17;
+int b = 4;
+
+int quotient = a / b;
+int remainder = a % b;
+{
+    if (a == 17 && b == 4) ;
+    Console.WriteLine($"Write out the results in the following form");
+    Console.WriteLine($"17/4 is 4 remainder 1");
+}
